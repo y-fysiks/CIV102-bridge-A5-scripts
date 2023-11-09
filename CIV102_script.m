8136882a0dd750faa99c04640d48c59cfa4db7f8
Yubo Wang <yubo0809@outlook.com>
2023-11-09 11:33:39 -0500

    Update CIV102_script.m

--- CIV102_script.m
+++ CIV102_script.m
@@ -11,12 +11,21 @@ xsection1 = [0, 75 + 1.27, 100, 75;
             10 + 1.27, 1.27, 90 - 1.27, 0; % bottom center
             85 - 1.27, 75, 90, 75 - 1.27;
             90 - 1.27, 75 - 1.27, 90, 0];
+xsection2 = [0, 75 + 1.27, 100, 75;
+            10, 75, 15 + 1.27, 75 - 1.27;
+            10, 75 - 1.27, 10 + 1.27, 0;
+            10 + 1.27, 1.27, 90 - 1.27, 0; % bottom center
+            85 - 1.27, 75, 90, 75 - 1.27;
+            90 - 1.27, 75 - 1.27, 90, 0];
 
-xsections = [xsection1]; % list of all cross sections
+xsections = {xsection1}; % list of all cross sections
 
 xsectionchanges = []; % define where the x section change is. Size is n - 1
 
 numxsections = length(xsections);
+
+glueheights = [75];
+numglues = 1;
 
 %% 1. SFD, BMD under train loading
 x_train = [52 228 392 568 732 908]; % Train Load Locations
@@ -43,3 +52,81 @@ for i = 1:n_train
 end
 SFD = max(abs(SFDi)); % SFD envelope
 BMD = max(BMDi); % BMD envelope
+
+
+%% 2. Geometric properties of cross sections
+
+
+
+ybars = zeros(1, numxsections);
+Is = zeros(1, numxsections);
+QCent = zeros(1, numxsections);
+Qglues = zeros(numxsections, numglues);
+
+for i = 1:numxsections
+    % ybar, I, Qcent
+    % sum the areas times distance to centroid of each individual square
+    sumAD = 0;
+    sumAreas = 0;
+    for j = 1:length(xsections{i})
+        % ybar
+        ax = xsections{i}(j, 1);
+        ay = xsections{i}(j, 2);
+        bx = xsections{i}(j, 3);
+        by = xsections{i}(j, 4);
+        sumAD = sumAD + (bx - ax) * (ay - by) * (by + ay) / 2;
+        sumAreas = sumAreas + (bx - ax) * (ay - by);
+
+    end
+    ybars(i) = sumAD / sumAreas;
+
+    % I
+    % bh^3/12 for every bit
+    Itot = 0;
+    for j = 1:length(xsections{i})
+        y = (by + ay) / 2;
+        h = (ay - by);
+        b = (bx - ax);
+        I = b * h^3 / 12;
+        ay2 = b * h * y^2;
+        Itot = Itot + I + ay2;
+    end
+    Is(i) = Itot;
+    
+    % all Qs
+    Qc = 0;
+    Qgs = zeros(1, numglues);
+    for j = 1:length(xsections{i})
+        % calculate area above centroidal axis of the jth shape, as well as
+        % centroid position relative to y axis
+        ax = xsections{i}(j, 1);
+        ay = xsections{i}(j, 2);
+        bx = xsections{i}(j, 3);
+        by = xsections{i}(j, 4);
+        if ay > ybars(i)
+            area = (bx - ax) * (ay - ybars(i));
+            centY = (ay + ybars(i)) / 2;
+            Qc = Qc + area * (centY - ybars(i));
+        end
+    end
+    for j = 1:numglues
+        % calculate Q from top
+        Qtop = 0;
+        for k = 1:length(xsections{i})
+            ax = xsections{i}(j, 1);
+            ay = xsections{i}(j, 2);
+            bx = xsections{i}(j, 3);
+            by = xsections{i}(j, 4);
+            if ay > glueheights(j)
+                area = (bx - ax) * (ay - glueheights(j));
+                centY = (ay + glueheights(j)) / 2;
+                Qtop = Qtop + area * (centY - glueheights(j));
+            end
+        end
+        Qgs(j) = Qtop;
+    end
+    QCent(i) = Qc;
+    Qglues(i) = Qgs;
+end
+
+
